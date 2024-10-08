af200b451a53ecb446d190e4ab7713733c07644d
ThatLoganJohnston <141460705+ThatLoganJohnston@users.noreply.github.com>
2024-10-08 09:17:14 -0600

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,31 +1,17 @@
 import express from 'express';
-import cors from 'cors';
-import fetch from 'node-fetch';
-import path from 'path'; // Add this line
+import path from 'path';
 
 const app = express();
-const PORT = 3000;
-
-app.use(cors());
-
-app.get('/api', async (req, res) => {
-    try {
-        const response = await fetch('https://paceman.gg/stats/api/getRecentRuns/?name=That_Logan_Guy&hours=24&limit=1');
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        res.status(500).send('Error fetching data');
-    }
-});
+const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(import.meta.url, 'public')));
+app.use(express.static(path.join(new URL('.', import.meta.url).pathname, 'public')));
 
 // Serve index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(import.meta.url, 'public', 'index.html'));
+    res.sendFile(path.join(new URL('.', import.meta.url).pathname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Proxy server is running at http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
